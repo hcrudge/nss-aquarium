86513b885953be3e38beb4f34a082976567cf449
hcrudge <hcrudge1@gmail.com>
2021-05-27 09:45:40 -0500

    addobjects

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,40 @@
+const database = {
+    fish:[
+    {
+        species: "Sunfish",
+        length: "24 inches",
+        name: "Sunny",
+        harvested: "California, USA",
+        diet: "Algae"
+    },
+    {
+        species: "Barracuda",
+        length: "60 inches",
+        name: "Bart",
+        harvested: "Havana, Cuba",
+        diet: "Anchovies",
+    },
+    {
+        species: "Sunfish",
+        length: "24 inches",
+        name: "Sunny",
+        harvested: "California, USA",
+        diet: "Algae",
+    },
+    {
+        species: "Pufferfish",
+        length: "12 inches",
+        name: "Puffy",
+        harvested: "Hawaii, USA",
+        diet: "Snails",
+    },
+    { 
+        species: "Lionfish" ,
+        length: "15 inches" ,
+        name: "Lenny" ,
+        harvested: "Miami, USA" ,
+        diet:  "crustaceans" ,
+    }
+               
+    ]
+}
